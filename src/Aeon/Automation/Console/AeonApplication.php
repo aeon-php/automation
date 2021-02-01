a425bd1d03250073644b9fb64d04ddca11ba4bc4
Norbert Orzechowicz <norbert@orzechowicz.pl>
2021-02-01 20:39:44 +0100

    Drop all options shortcuts

--- src/Aeon/Automation/Console/AeonApplication.php
+++ src/Aeon/Automation/Console/AeonApplication.php
@@ -16,14 +16,14 @@ final class AeonApplication extends Application
         $definition->addOption(
             new InputOption(
                 'configuration',
-                'c',
+                null,
                 InputOption::VALUE_REQUIRED,
                 'Custom path to the automation.xml configuration file.'
             )
         );
         $definition->addOption(new InputOption(
             'cache-path',
-            'cp',
+            null,
             InputOption::VALUE_REQUIRED,
             'Path to root cache directory, taken from sys_get_tmp_dir() function or <fg=yellow>AEON_AUTOMATION_CACHE_DIR</> env variable',
             \getenv('AEON_AUTOMATION_CACHE_DIR')
@@ -40,13 +40,13 @@ final class AeonApplication extends Application
         ));
         $definition->addOption(new InputOption(
             'github-token',
-            'gt',
+            null,
             InputOption::VALUE_REQUIRED,
             'Github personal access token, generated here: https://github.com/settings/tokens By default taken from <fg=yellow>AEON_AUTOMATION_GH_TOKEN</> env variable'
         ));
         $definition->addOption(new InputOption(
             'github-enterprise-url',
-            'geu',
+            null,
             InputOption::VALUE_REQUIRED,
             'Github enterprise URL, by default taken from <fg=yellow>AEON_AUTOMATION_GH_ENTERPRISE_URL</> env variable',
             \getenv('AEON_AUTOMATION_GH_ENTERPRISE_URL') ? \getenv('AEON_AUTOMATION_GH_ENTERPRISE_URL') : null
