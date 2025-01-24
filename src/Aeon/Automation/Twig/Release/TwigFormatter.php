2fc21d7aa35f55f22cf63d10d9cccdcd06a079aa
Norbert Orzechowicz <contact@norbert.tech>
2025-01-23 22:10:02 -0600

    Updated dependencies

--- src/Aeon/Automation/Twig/Release/TwigFormatter.php
+++ src/Aeon/Automation/Twig/Release/TwigFormatter.php
@@ -18,6 +18,8 @@ final class TwigFormatter implements Formatter
 
     private string $theme;
 
+    private bool $footer;
+
     public function __construct(string $rootDir, string $format, string $theme)
     {
         $format = \strtolower($format);
